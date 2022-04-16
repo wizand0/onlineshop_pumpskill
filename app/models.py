5e0562b5e57b9b53ce445df4d1ec0e555d58e469
Makarov Andrey <xabor@yandex.ru>
2022-04-16 14:04:59 +0300

    Добавление моделей

--- app/models.py
+++ app/models.py
@@ -1,6 +1,17 @@
+from datetime import datetime
+
 from app import db
 
 
 class Brand(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(250))
+    items = db.relationship('Item', backref='brand', cascade='all,delete')
+
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250))
+    price = db.Column(db.Numeric(9, 2))
+    created = db.Column(db.DateTime, default=datetime.utcnow)
+    brand_id = db.Column(db.Integer, db.ForeignKey('brand.id'))
