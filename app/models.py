ff7c242c2fba75b3244030a90cb1acd23f73fa7b
Makarov Andrey <xabor@yandex.ru>
2022-04-17 07:43:36 +0300

    Выполнение ДЗ v0.1

--- app/models.py
+++ app/models.py
@@ -18,6 +18,19 @@ class Item(db.Model):
     price = db.Column(db.Float(9, 2))
     created = db.Column(db.DateTime, default=datetime.utcnow)
     brand_id = db.Column(db.Integer, db.ForeignKey('brand.id'))
+    users_items = db.relationship('UsersItem', backref='item')
 
     def __repr__(self):
         return self.title
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250))
+    users_items = db.relationship('UsersItem', backref='user', cascade='all,delete')
+
+
+class UsersItem(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    item_id = db.Column(db.Integer, db.ForeignKey('item.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
